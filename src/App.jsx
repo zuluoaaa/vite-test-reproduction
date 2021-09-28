a6b6821d4f9d52e601a920da6b2e1f3474b4d6ce
wzb <864457005@qq.com>
2021-09-28 16:58:04 +0800

    feat

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState,Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { connect } from "react-redux";
+import React from 'react'
+
+@connect
+class c extends Component{
+
+}
+
+class App extends Component{
+  render(){
+    return (
+        <div>
+          <h1>hi</h1>
+        </div>
+    )
+  }
+}
+
+export default App
